9b0c9e2f329ed94b88ebee7f357f4b5f78f62b9e
Axner <@axnerhagglund@gmail.com>
2025-04-14 11:38:04 +0200

    s

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,40 @@
 import "./header.css";
 import logo from "../../assets/rmdb_logo.png";
-// import { Link } from "react-router-dom";
+
+
 
 export default function Header() {
+ 
+  
   const handleForm = (formData) => {
     const search = formData.get("searchInput");
-    console.log(search);
-    // useFetch()
+   console.log(search)
   };
 
+ 
+ 
+
+  
+
   return (
+    
     <header className="header">
       <div className="header__logo">
         <img src={logo} alt="RMDB logo" width={352} height={110} />
       </div>
-      <form action="handleForm" className="header__form">
-        <input type="text" className="header__form__input" id="searchInput" />
+      <form action={handleForm} className="header__form">
+        <input type="text" className="header__form__input" name="searchInput" />
         <button type="submit" className="header__form__btn">
           Search title
         </button>
       </form>
       <p className="header__link">Favourites</p>
     </header>
+
+    
+
+   
+ 
+   
   );
 }
