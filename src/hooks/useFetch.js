8b05ddbd8a8b7214553a52086d247f31c14b76ef
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-04-14 17:12:33 +0200

    basic search working

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,37 +2,37 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-  
-    useEffect(() => {
-      const controller = new AbortController(); // 🆕 create a controller
-      const signal = controller.signal;
-        
-      setIsLoading(true);
-      setIsError(false);
-  
-      axios.get(url, { signal }) // 🆕 pass signal to axios
-        .then((response) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    setIsLoading(true);
+    setIsError(false);
+
+    axios
+      .get(url, { signal })
+      .then((response) => {
         setData(response.data);
-        })
-       .catch((error) => {
-          if (axios.isCancel(error)) {
-            // 🆗 fetch was cancelled on purpose
-            console.log("Request cancelled:", error.message);
-          } else {
-            setIsError(true);
-          }
-        })
-       .finally(() => {
-           setIsLoading(false);
-        });
-  
-        return () => {
-          controller.abort(); // 🧹 cleanup: cancel the request if still running
-        };
-      }, [url]);
-  
-    return { data, isLoading, isError };
-  };
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log("Request cancelled:", error.message);
+        } else {
+          setIsError(true);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isLoading, isError };
+};
--- src/pages/SearchResultPage/SearchResultPage.jsx
+++ src/pages/SearchResultPage/SearchResultPage.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { useOutletContext, useSearchParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import MovieGrid from "../../components/MovieGrid/MovieGrid";
 
 function SearchResultPage() {
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get("q");
-  const { data: movies, loading: moviesLoading } = useFetch(
-    "https://santosnr6.github.io/Data/favoritemovies.json"
-  );
+  const { searchUrl } = useOutletContext();
+  //const { data, isLoading, isError } = useFetch(searchUrl);
+  //const [searchParams] = useSearchParams();
+  //const query = searchParams.get("q");
+  const { data: movies, loading: moviesLoading, isError } = useFetch(searchUrl);
+
+  console.log(movies.Search);
 
   if (moviesLoading) return <h1 className="loadingText">Loading...</h1>;
 
   return (
     <main className="searchresultpage">
-      <MovieGrid movies={movies} />
+      {<MovieGrid movies={movies.Search} />}
     </main>
   );
 }
