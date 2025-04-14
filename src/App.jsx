00bec5aea4835e1bdf0c3fddc4166a701dfe9045
Senja Erlandsson <senjaerlandsson@gmail.com>
2025-04-14 10:06:52 +0200

    merga header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Search from "./search";
+import Home from "./home";import "./App.css";
 import Header from "./components/header/Header";
 
 function App() {
   return (
-    <section className="app">
-      <Header />
-      <h1>StartPage</h1>
-    </section>
-  );
+  <>
+  <nav>
+    <ul>
+    <li>
+        <Link to="/">home</Link>
+      </li>
+      <li>
+        <Link to="/search">search</Link>
+      </li>
+    </ul>
+  </nav>
+   <Routes>
+    <Route path="/" element={ <Home /> } />
+    <Route path="/search" element={ <Search />}> 
+     {/*  <Route path=":id" element={ <Search /> } /> */}
+    </Route>
+    <Route path="*" element={<Error /> } />
+   </Routes>
+  </>
+  )
 }
 
 export default App;
+
+{/* <section className='app'>
+      <h1>StartPage</h1>
+   </section>  */}
