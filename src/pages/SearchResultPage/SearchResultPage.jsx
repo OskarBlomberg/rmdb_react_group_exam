f10d12d255ba2321e69fc739f175d05d3ed2a832
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-04-15 10:54:03 +0200

    search function

--- src/pages/SearchResultPage/SearchResultPage.jsx
+++ src/pages/SearchResultPage/SearchResultPage.jsx
@@ -5,18 +5,18 @@ import MovieGrid from "../../components/MovieGrid/MovieGrid";
 
 function SearchResultPage() {
   const { searchUrl } = useOutletContext();
-  //const { data, isLoading, isError } = useFetch(searchUrl);
-  //const [searchParams] = useSearchParams();
-  //const query = searchParams.get("q");
   const { data: movies, loading: moviesLoading, isError } = useFetch(searchUrl);
 
-  console.log(movies.Search);
-
-  if (moviesLoading) return <h1 className="loadingText">Loading...</h1>;
+  if (moviesLoading)
+    return (
+      <main className="mainLoading">
+        <h1 className="loadingText">Loading...</h1>
+      </main>
+    );
 
   return (
     <main className="searchresultpage">
-      {<MovieGrid movies={movies.Search} />}
+      {movies.Search && <MovieGrid movies={movies.Search} />}
     </main>
   );
 }
