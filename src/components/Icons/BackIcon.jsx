5ccce04c9e3bb8dfbf746c2ab9c09cfe214541fc
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-04-16 10:04:04 +0200

    bugfixes

--- src/components/Icons/BackIcon.jsx
+++ src/components/Icons/BackIcon.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import "./BackIcon.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./backIcon.css";
+import { Link } from "react-router-dom";
 function BackIcon() {
   return (
     <Link to={"/"}>
-    <button className='back-button'>Back</button>
+      <button className="back-button">Back</button>
     </Link>
-    
-  )
+  );
 }
 
-export default BackIcon
+export default BackIcon;
